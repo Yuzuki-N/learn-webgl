91a2c0917bbf65159e95734915e5459f65f82448
unknown <nanami2962@gmail.com>
2022-03-24 14:43:47 +0800

    tired

---
+++ ch5/5.3-MultiAttributeColor/MultiAttributeColor.js
@@ -0,0 +1,91 @@
+let VSHADER_SOURCE = `
+    attribute vec4 a_Position;
+    attribute vec4 a_Color;
+    varying vec4 v_Color;
+    void main() {
+        gl_Position = a_Position;
+        gl_PointSize = 10.0;
+        v_Color = a_Color;
+    }
+`;
+
+let FSHADER_SOURCE = `
+    precision mediump float;
+    varying vec4 v_Color;
+    void main() {
+        gl_FragColor = v_Color;
+    }
+`;
+
+
+let initVertexBuffers = (gl) => {
+    let verticesColors = new Float32Array([
+        // Vertex coordinates and color
+         0.0,  0.5,  1.0,  0.0,  0.0, 
+        -0.5, -0.5,  0.0,  1.0,  0.0, 
+         0.5, -0.5,  0.0,  0.0,  1.0, 
+      ]);
+
+    let n = 3;
+
+    let vertexColorBuffer = gl.createBuffer();
+    if (!vertexColorBuffer) {
+        console.log('Failed to create the buffer object');
+        return false;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
+
+    let FSIZE = verticesColors.BYTES_PER_ELEMENT;
+
+    let a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return -1;
+    }
+
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0);
+    gl.enableVertexAttribArray(a_Position);
+
+    let a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+    if(a_Color < 0) {
+      console.log('Failed to get the storage location of a_Color');
+      return -1;
+    }
+
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2);
+    gl.enableVertexAttribArray(a_Color);  
+
+    return n;
+};
+
+function main() {
+    let canvas = document.getElementById('webgl');
+    let gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+      }
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
+
+    let n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the vertex information');
+        return;
+    }
+
+    // Specify the color for clearing <canvas>
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Draw three points
+    gl.drawArrays(gl.POINTS, 0, n);
+    // gl.drawArrays(gl.TRIANGLES, 0, n);
+
+}
